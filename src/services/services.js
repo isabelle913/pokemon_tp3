0db467663481ad4db1a7ec61d16f0cee84c03137
isabelle913 <atelierisabelle@videotron.ca>
2024-01-27 13:42:25 -0500

    start services

--- src/services/services.js
+++ src/services/services.js
@@ -1,9 +1,33 @@
 import axios from "axios";
+import { usePokemonsStore } from "../stores/pokemon";
 
-export default function useServices() {
-  const api = axios.create({});
+export function useServices() {
+  const { addPokemon, sortPokemon } = usePokemonsStore();
 
-  return { api };
+  const api = axios.create({ baseURL: "https://pokeapi.co/api/v2/" });
+
+  function requestPokemon(url) {
+    console.log("requestPokemon");
+
+    api
+      .get(url)
+      .then((resp) => {
+        console.log("resp", resp.data.results);
+        resp.data.results.map((pokemon) => {
+          addPokemon(pokemon);
+        });
+        sortPokemon();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  return { requestPokemon };
 }
 //https://pokeapi.co/api/v2/
 // pokemon/?limit=1500&offset=0"
+
+//https://pokeapi.co/api/v2/pokemon/?limit=15&offset=0
+
+// const url = "pokemon/?limit=1500&offset=0";
