c8ac76ba3b05d2a00d77b8d269b6aea6c195a30a
isabelle913 <atelierisabelle@videotron.ca>
2024-01-27 13:41:59 -0500

    do store

--- src/stores/pokemon.js
+++ src/stores/pokemon.js
@@ -2,8 +2,25 @@ import { ref } from "vue";
 import { defineStore } from "pinia";
 
 export const usePokemonsStore = defineStore("pokemonStore", () => {
-  const pokemons = ref(["Coucou"]);
+  const pokemons = ref([]);
   const pokemonFavorites = ref([]);
 
-  return { pokemons, pokemonFavorites };
+  function getUnikID() {
+    return Date.now() + Math.trunc(Math.random() * 10000);
+  }
+
+  function addPokemon(pokemon) {
+    this.pokemons.push({
+      id: getUnikID(),
+      name: pokemon.name,
+      url: pokemon.url,
+      isLoader: false,
+    });
+  }
+
+  function sortPokemon() {
+    console.log("sortPokemon");
+  }
+
+  return { pokemons, pokemonFavorites, addPokemon, sortPokemon };
 });
