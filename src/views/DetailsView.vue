96b7588582fb2eef51d963b7026c913582cd52fd
isabelle913 <atelierisabelle@videotron.ca>
2024-02-01 18:31:23 -0500

    started to coonection with store

--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -1,7 +1,53 @@
 <template>
   <div>Details</div>
+  <div>Pokemon: {{ pokemon }}</div>
 </template>
+
 <script setup>
-// import { ref } from "vue";
+import { computed, onMounted } from "vue";
+import { usePokemonsStore } from "../stores/pokemon";
+import { useRoute, useRouter } from "vue-router";
+
+const store = usePokemonsStore();
+const route = useRoute();
+const router = useRouter();
+
+const id = route.params.id;
+
+// TODO transférer le load des pokemon dans app
+const pokemon = computed(() => {
+  let kleenex = store.pokemons.map((pokemon) => {
+    console.log("pokemon", pokemon);
+
+    pokemon.value.id === id;
+  });
+  return kleenex;
+});
+
+function onFavorite(id) {
+  const pokemon = store.pokemons.find((pokemon) => pokemon.id === id);
+  pokemon.isFavorite = !pokemon.isFavorite;
+}
+
+// function goToDetails(id) {
+//   router.push({ name: "pokemon", params: { id: id } });
+// }
+
+// function loadPokemon() {
+//   // const id = route.params.id;
+//   console.log("id", id);
+//   if (!id) router.push({ name: home });
+// }
+
+onMounted(() => {
+  console.log("id", id);
+  if (!id) router.push({ name: home });
+});
 </script>
-<style></style>
+
+<style scoped>
+img {
+  width: 100%;
+  height: 100%;
+}
+</style>
