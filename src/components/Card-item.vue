c9e59d4ecf75ade823a83004235f0b5be91f83cb
isabelle913 <atelierisabelle@videotron.ca>
2024-02-10 13:42:19 -0500

    create and used component pok-chip

--- src/components/Card-item.vue
+++ src/components/Card-item.vue
@@ -1,6 +1,5 @@
 <template>
-  <!-- prettier-ignore -->
-  <div class="flex flex-col items-center basis-60 grow rounded-lg sm:shadow h-[380px] p-2 cursor-pointer" @click="goToDetails(pokemon.id)"> 
+  <div class="flex flex-col items-center basis-60 grow rounded-lg sm:shadow h-[380px] p-2 cursor-pointer" @click="goToDetails(pokemon.id)">
     <div class="h-3/4">
       <img class="" :src="pokemon.img" />
     </div>
@@ -10,28 +9,31 @@
           {{ pokemon.displayName }}
         </div>
         <div class="col-span-2 group flex">
-          <!-- prettier-ignore -->
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" @click.stop="onFavorite(pokemon.id)" class="w-10 h-10 cursor-pointer"
-            :class="pokemon.isFavorite ? 'fill-red-500 stroke-red-500' : 'stroke-black' ">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            @click.stop="onFavorite(pokemon.id)"
+            class="w-10 h-10 cursor-pointer"
+            :class="pokemon.isFavorite ? 'fill-red-500 stroke-red-500' : 'stroke-black'">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
           </svg>
         </div>
       </div>
 
-      <div class="flex justify-center gap-2 pt-2">
-        <!-- prettier-ignore -->
-        <div v-for="(type, index) in pokemon.types" :key="index" class="rounded-lg w-[70px] py-1 text-center" :style="{ backgroundColor: `${type.color}` }" >
-          {{ type.displayName }}
-        </div>
-      </div>
+      <pok-chips :types="pokemon.types"></pok-chips>
     </div>
-
   </div>
 </template>
 
 <script setup>
 import { usePokemonsStore } from "../stores/pokemon";
 import { useRouter } from "vue-router";
+import pokChips from "./pok-chips.vue";
 
 const store = usePokemonsStore();
 const router = useRouter();
@@ -42,7 +44,6 @@ const props = defineProps({
     required: true,
   },
 });
-
 function onFavorite(id) {
   const pokemon = store.pokemons.find((pokemon) => pokemon.id === id);
   pokemon.isFavorite = !pokemon.isFavorite;
---
+++ src/components/pok-chips.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex gap-2 pt-2" :class="classAlign">
+    <div v-if="types" v-for="(type, index) in types" :key="index" class="rounded-lg w-[70px] py-1 text-center" :style="{ backgroundColor: `${type.color}` }">
+      {{ type.displayName }}
+    </div>
+
+    <div v-if="abilities" v-for="(ability, index) in abilities" :key="index" class="rounded-lg px-3 py-1 text-center" :style="getColor()">
+      {{ ability }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  types: {
+    type: Array,
+    required: false,
+  },
+  abilities: {
+    type: Array,
+    required: false,
+  },
+  classAlign: {
+    type: String,
+    default: "justify-center",
+  },
+});
+
+const colors = ["#9EB7B8", "#D56723", "#3DC7EF", "#B97FC9", "#F7DE3F", "#A38C21", "#729F3F", "#7B62A3"];
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getColor() {
+  const index = getRandomNumber(0, 7);
+  return { backgroundColor: colors[index] };
+}
+</script>
